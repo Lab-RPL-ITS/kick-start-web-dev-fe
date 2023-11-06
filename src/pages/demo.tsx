4406a594b239a98fb2ff196cd1a64d481ca95bd0
Gabbynts <gabriella.natasya2003@gmail.com>
2023-11-06 23:59:50 +0700

    feat: add landing page

---
+++ src/pages/demo.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="bg-blue-500 p-[10px] rounded-lg">
+      <p className="text-white">
+        Ini adalah teks dengan latar belakang biru, padding 4, dan sudut
+        melengkung.
+      </p>
+    </div>
+  );
+}
